a14ed1f95f27d92ba4702d092a2f344edd4910b5
Damon Toumbourou <dtoumbourou@gmail.com>
2016-02-17 18:58:10 +1100

    ready for repo

--- resources/lib/c31scraper.py
+++ resources/lib/c31scraper.py
@@ -24,11 +24,10 @@ def get_content(url, keyword):
             for k in found_content:
                 label_path = k.find('h3', {'class': 'yt-lockup-title '})
                 label = label_path.find('a').get('title')
-                print label
 
                 path = label_path.find('a').get('href')
                 path = re.search(r'\=(.*)', path).group(0)
-
+            
                 thumb = k.find('img')['data-thumb']
                 thumb = 'http:' + thumb
                 
@@ -41,4 +40,29 @@ def get_content(url, keyword):
                 output.append(item)
 
     return output
-get_content('https://www.youtube.com/user/channel31melbourne', 'C31 How To... Series')
+
+
+def get_alt_content(url):
+    soup = get_soup(url)
+    content = soup.find_all('div', {'class': 'yt-lockup-dismissable'})
+
+    output = []
+    for i in content:
+        label_path = i.find('h3', {'class': 'yt-lockup-title '}).find('a')
+        label = label_path.get_text()
+
+        path = label_path.get('href')
+        path = re.search(r'\=(.*)', path).group(0)
+
+        thumb = i.find('img')['src']
+        thumb = 'http' + thumb
+        
+        item = { 
+            'label': label,
+            'path': path,
+            'thumbnail': thumb,
+        }
+
+        output.append(item)
+        
+    return output
